0a68848f0e8b9df7ea6557e083c91da6af427ee4
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-22 21:59:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 
   data.each_with_object({}) do |(keys, value), final_array|
-    binding.pry
- final_array[k] = v
+ value.each do |inner_keys, names|
   binding.pry
 end
+final_array
 end
