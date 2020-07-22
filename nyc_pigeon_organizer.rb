84ee24749705929a32f5f69b370c466d1aa137b3
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-22 22:10:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,16 +4,17 @@ def nyc_pigeon_organizer(data)
 final_results =  data.each_with_object({}) do |(keys, value), final_array|
  value.each do |inner_keys, names|
   names.each do |name|
-    binding.pry
+    #binding.pry
     if !final_array[name]
       final_array[name] = {}
     end
  if !final_array[name][key]
    final_array[name][key] = []
-  end
+      end
   final_array[name][key].push(inner_keys)
-  end
-end
+      end
+    end
 final_array
-end
+  end
+  binding.pry
 end
